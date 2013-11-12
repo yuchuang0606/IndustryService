d64671e8b13632b11790d69573059af1e2aaf58b
zyb <YHW@192.168.1.153>
2013-11-12 21:15:13 +0800

    zyb 21:15

---
+++ IndustryService/src/model/Group.java
@@ -0,0 +1,43 @@
+package model;
+
+public class Group {
+	private int groupid;
+	private String groupname;
+	private String groupdesribe;
+	/**
+	 * @return the groupid
+	 */
+	public int getGroupid() {
+		return groupid;
+	}
+	/**
+	 * @param groupid the groupid to set
+	 */
+	public void setGroupid(int groupid) {
+		this.groupid = groupid;
+	}
+	/**
+	 * @return the groupname
+	 */
+	public String getGroupname() {
+		return groupname;
+	}
+	/**
+	 * @param groupname the groupname to set
+	 */
+	public void setGroupname(String groupname) {
+		this.groupname = groupname;
+	}
+	/**
+	 * @return the groupdesribe
+	 */
+	public String getGroupdesribe() {
+		return groupdesribe;
+	}
+	/**
+	 * @param groupdesribe the groupdesribe to set
+	 */
+	public void setGroupdesribe(String groupdesribe) {
+		this.groupdesribe = groupdesribe;
+	}
+}
---
+++ IndustryService/src/model/User.java
@@ -0,0 +1,139 @@
+/**
+ * @authored by zhouyunbin
+ * @createdate 2013-11-14
+ */
+package model;
+import java.util.Date;
+public class User {
+	private int userid;
+	private String username;
+	private String password;
+	private int usergroup;
+	private String phone;
+	private String qq;
+	private String address;
+	private String userpic;
+	private Date regdate;
+	private String email;
+	/**
+	 * @return the userid
+	 */
+	public int getUserid() {
+		return userid;
+	}
+	/**
+	 * @param userid the userid to set
+	 */
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the usergroup
+	 */
+	public int getUsergroup() {
+		return usergroup;
+	}
+	/**
+	 * @param usergroup the usergroup to set
+	 */
+	public void setUsergroup(int usergroup) {
+		this.usergroup = usergroup;
+	}
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	/**
+	 * @return the qq
+	 */
+	public String getQq() {
+		return qq;
+	}
+	/**
+	 * @param qq the qq to set
+	 */
+	public void setQq(String qq) {
+		this.qq = qq;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the userpic
+	 */
+	public String getUserpic() {
+		return userpic;
+	}
+	/**
+	 * @param userpic the userpic to set
+	 */
+	public void setUserpic(String userpic) {
+		this.userpic = userpic;
+	}
+	/**
+	 * @return the regdate
+	 */
+	public Date getRegdate() {
+		return regdate;
+	}
+	/**
+	 * @param regdate the regdate to set
+	 */
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
